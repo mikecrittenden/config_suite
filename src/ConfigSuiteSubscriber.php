d78461beea33e503be655b0f6642b20c6ad09e2a
Greg Boggs <greg.boggs@thinkshout.com>
2017-12-02 15:55:34 -0800

    full rewrite on delete until our per delete PR is working.

--- src/ConfigSuiteSubscriber.php
+++ src/ConfigSuiteSubscriber.php
@@ -5,11 +5,22 @@ namespace Drupal\config_suite;
 use Drupal\system\SystemConfigSubscriber;
 use Drupal\Core\Config\ConfigImporterEvent;
 use Drupal\Core\Config\ConfigCrudEvent;
+use Drupal\Core\Config\ConfigEvents;
 
 /**
  * System Config subscriber.
  */
 class ConfigSuiteSubscriber extends SystemConfigSubscriber {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[ConfigEvents::SAVE][] = ['onConfigSave', 255];
+    $events[ConfigEvents::DELETE][] = ['onConfigDelete', 255];
+
+    return $events;
+  }
 
   /**
    * Ignores the check that the configuration synchronization is from the same site.
@@ -26,8 +37,28 @@ class ConfigSuiteSubscriber extends SystemConfigSubscriber {
     return true;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function onConfigSave(ConfigCrudEvent $event) {
+    $this->updateConfig($event);
+  }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function onConfigDelete(ConfigCrudEvent $event) {
+
+    // To handle deletes, we delete and rewrite the full configuration.
+    $this->replaceConfig();
+  }
+
+  /**
+   * Update config with only the changed configuration.
+   *
+   * @param \Drupal\Core\Config\ConfigCrudEvent $event
+   */
+  private function updateConfig(ConfigCrudEvent $event) {
     $config = \Drupal::config('config_suite.settings');
     if (!$config->get('automatic_export')) {
       return;
@@ -42,7 +73,10 @@ class ConfigSuiteSubscriber extends SystemConfigSubscriber {
     $changed_config = $config->getName();
 
     //If the config is a config split change, we must rebuild the full config.
-    if (substr($changed_config, 0, 13) !== 'config_split.') {
+    if (substr($changed_config, 0, 13) === 'config_split.' ) {
+      $this->replaceConfig();
+    }
+    else {
       $sync_storage->write($changed_config, $active_storage->read($changed_config));
 
       // Export configuration collections.
@@ -52,21 +86,28 @@ class ConfigSuiteSubscriber extends SystemConfigSubscriber {
         $sync_collection->write($changed_config, $active_collection->read($changed_config));
       }
     }
-    else {
-      $sync_storage->deleteAll();
+  }
 
-      // Write all .yml files.
-      foreach ($active_storage->listAll() as $name) {
-        $sync_storage->write($name, $active_storage->read($name));
-      }
+  /**
+   * Wipe and replace the configuration.
+   */
+  private function replaceConfig() {
+    $sync_storage = \Drupal::service('config.storage.sync');
+    $active_storage = \Drupal::service('config.storage');
 
-      // Export configuration collections.
-      foreach ($active_storage->getAllCollectionNames() as $collection) {
-        $active_collection = $active_storage->createCollection($collection);
-        $sync_collection = $sync_storage->createCollection($collection);
-        foreach ($active_collection->listAll() as $name) {
-          $sync_collection->write($name, $active_collection->read($name));
-        }
+    $sync_storage->deleteAll();
+
+    // Write all .yml files.
+    foreach ($active_storage->listAll() as $name) {
+      $sync_storage->write($name, $active_storage->read($name));
+    }
+
+    // Export configuration collections.
+    foreach ($active_storage->getAllCollectionNames() as $collection) {
+      $active_collection = $active_storage->createCollection($collection);
+      $sync_collection = $sync_storage->createCollection($collection);
+      foreach ($active_collection->listAll() as $name) {
+        $sync_collection->write($name, $active_collection->read($name));
       }
     }
   }
